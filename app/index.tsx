ab5f5eed6d5d1bfe96d669ef141e16ae17d4ae2c
Brayden O'Neal <braydenoneal@protonmail.com>
2024-11-17 18:21:05 -0600

    Work on camera stuff

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,20 @@
-import { CameraView, CameraType, useCameraPermissions } from 'expo-camera'
-import { useState } from 'react'
-import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { CameraView, useCameraPermissions } from 'expo-camera'
+import { Button, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { useRef, useState } from 'react'
+
+import { getUrl } from '@/lib/url'
 
 export default function App() {
-    const [facing, setFacing] = useState<CameraType>('back')
     const [permission, requestPermission] = useCameraPermissions()
+    const [image, setImage] = useState<string>()
+
+    const cameraRef = useRef<CameraView>(null)
 
     if (!permission) {
-        // Camera permissions are still loading.
         return <View/>
     }
 
     if (!permission.granted) {
-        // Camera permissions are not granted yet.
         return (
             <View style={styles.container}>
                 <Text style={styles.message}>We need your permission to show the camera</Text>
@@ -21,19 +23,76 @@ export default function App() {
         )
     }
 
-    function toggleCameraFacing() {
-        setFacing(current => (current === 'back' ? 'front' : 'back'))
+    async function takePicture() {
+        if (cameraRef.current) {
+            const data = await cameraRef.current.takePictureAsync()
+            if (data) {
+                setImage(data.uri)
+            }
+        }
+    }
+
+    async function sendImage() {
+        // const body = new FormData()
+        // body.append('image', { uri: image, name: 'photo.jpg', type: 'image/jpeg' })
+        // body.append('Content-Type', 'image/jpeg')
+
+        try {
+            // const data = await fetch('http://10.0.2.2:8000')
+            // const data = await fetch(await getUrl())
+            // const data = await fetch('https://drury.edu')
+            const data = await fetch('https://mcs.drury.edu/mirror/user', { method: 'POST', headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                }, })
+            console.log(data)
+            console.log(await data.json())
+        } catch (e) {
+            console.log(e)
+        }
+
+        // try {
+        //     const url = await getUrl()
+        //
+        //     const data = await fetch(url + '/image', {
+        //         method: 'POST',
+        //         headers: {
+        //             'Content-Type': 'multipart/form-data',
+        //         },
+        //         body,
+        //     })
+        //
+        //     console.log(await data.json())
+        // } catch (e) {
+        //     console.log(e)
+        // }
+    }
+
+    async function resetImage() {
+        setImage(undefined)
     }
 
     return (
         <View style={styles.container}>
-            <CameraView style={styles.camera} facing={facing}>
-                <View style={styles.buttonContainer}>
-                    <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
-                        <Text style={styles.text}>Flip Camera</Text>
+            {!image ? (
+                <View style={styles.camera}>
+                    <CameraView style={styles.camera} ref={cameraRef}>
+                    </CameraView>
+                    <TouchableOpacity style={styles.button} onPress={takePicture}>
+                        <Text style={styles.text}>Take picture</Text>
                     </TouchableOpacity>
                 </View>
-            </CameraView>
+            ) : (
+                <View style={styles.camera}>
+                    <Image source={{ uri: image }} style={styles.camera}/>
+                    <TouchableOpacity style={styles.button} onPress={sendImage}>
+                        <Text style={styles.text}>Upload image</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button} onPress={resetImage}>
+                        <Text style={styles.text}>Back</Text>
+                    </TouchableOpacity>
+                </View>
+            )}
         </View>
     )
 }
@@ -48,7 +107,7 @@ const styles = StyleSheet.create({
         paddingBottom: 10,
     },
     camera: {
-        flex: 1,
+        flex: 3,
     },
     buttonContainer: {
         flex: 1,
@@ -64,6 +123,6 @@ const styles = StyleSheet.create({
     text: {
         fontSize: 24,
         fontWeight: 'bold',
-        color: 'white',
+        color: 'black',
     },
 })
